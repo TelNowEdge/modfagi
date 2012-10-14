fc4be51e10be7a5262583a808405fe502c95e36c
Bernard Merindol <Bernard.Merindol@telnowedge.com>
2012-10-14 17:07:19 +0200

    Version 0.1.0

---
+++ functions.inc.php
@@ -0,0 +1,164 @@
+<?php /* $Id */
+
+function modfagi_getdest($id) {
+  $name=sql("SELECT displayname FROM fagi WHERE id='$id'","getOne");
+  return array('fagi,'.$name.',1');
+}
+
+function modfagi_getdestinfo($dest) {
+  global $active_modules;
+
+  if (substr(trim($dest),0,9) == 'ext-fagi,') {
+    $exten = explode(',',$dest);
+    $exten = $exten[1];
+    $thisexten = modfagi_list($exten);
+    if (empty($thisexten)) {
+      return array();
+    } else {
+      //$type = isset($active_modules['announcement']['type'])?$active_modules['announcement']['type']:'setup';
+      return array('description' => sprintf(_("FastAgi %s : %s"),$exten,$thisexten['description']),
+		   'edit_url' => 'config.php?isplay=fagi&itemid='.urlencode($thisexten['id']),
+		   );
+                }
+  } else {
+    return false;
+  }
+  
+}
+
+
+function modfagi_check_destinations($dest=true) {
+  global $active_modules;
+
+ $destlist = array();
+  if (is_array($dest) && empty($dest)) {
+    return $destlist;
+  }
+  $sql = "SELECT id,displayname,truegoto,falsegoto FROM fagi ";
+  if ($dest !== true) {
+    $sql .= "WHERE (truegoto in ('".implode("','",$dest)."') ) OR (falsegoto in ('".implode("','",$dest)."') )";
+  }
+  $results = sql($sql,"getAll",DB_FETCHMODE_ASSOC);
+
+  $type = isset($active_modules['modfagi']['type'])?$active_modules['modfagi']['type']:'setup';
+
+  foreach ($results as $result) {
+    $thisdest    = $result['truegoto'];
+    $thisid      = $result['id'];
+    $description = sprintf(_("Fast AGI: %s"),$result['displayname']);
+    $thisurl     = 'config.php?display=fagi&itemid='.urlencode($thisid);
+    if ($dest === true || $dest = $thisdest) {
+      $destlist[] = array(
+			  'dest' => $thisdest,
+			  'description' => $description,
+			  'edit_url' => $thisurl,
+			  );
+    }
+    $thisdest = $result['falsegoto'];
+    if ($dest === true || $dest = $thisdest) {
+      $destlist[] = array(
+			  'dest' => $thisdest,
+			  'description' => $description,
+			  'edit_url' => $thisurl,
+			  );
+    }
+  }
+  return $destlist;
+}
+
+// returns a associative arrays with keys 'destination' and 'description'
+function modfagi_destinations() {
+  //get the list of fagi
+  $results = modfagi_list();
+
+  // return an associative array with destination and description
+  if (isset($results)) {
+    foreach($results as $result){
+      $extens[] = array('destination' => 'fagi,'.$result['displayname'].',1', 'description' => $result['displayname']);
+    }
+
+    return $extens;
+  } else {
+    return null;
+  }
+}
+
+/* 	Generates dialplan for fagi
+	We call this with retrieve_conf
+*/
+function modfagi_get_config($engine) {
+  global $ext;  // is this the best way to pass this?
+  global $conferences_conf;
+
+  switch($engine) {
+  case "asterisk":
+    $agilist = modfagi_list();
+    if(is_array($agilist)) {
+      $context = "fagi";
+      foreach ($agilist as $item) {
+	// write out the dialplan details
+	$id=$item['displayname'];
+	$fagidesc=$item['description'];
+	$ext->add($context, $id, '', new ext_noop('Fast Agi: '.$fagidesc));
+	$ext->add($context, $id, '', new ext_setvar('FAGIRUN','OK'));
+	$agi="agi://".$item['host'].":".$item['port']."/".$item['path'];
+	if ($item['query'] != '') {$agi .="?".$item['query'];}
+	$ext->add($context, $id, '', new ext_agi($agi));
+	$ext->add($context, $id, '', new ext_gotoif('$["${FAGIRUN}"="NO"','notrun'));
+	list($cont,$exten,$prio)=explode(',',$item['truegoto']);
+	$ext->add($context, $id, '', new ext_goto($prio,$exten,$cont));
+	list($cont,$exten,$prio)=explode(',',$item['falsegoto']);
+	$ext->add($context, $id, 'notrun', new ext_goto($prio,$exten,$cont));
+      }
+    }
+    break;
+  }
+}
+
+function modfagi_list($displayname="") {
+  if ($displayname == "") {
+    $results = sql("SELECT * FROM fagi","getAll",DB_FETCHMODE_ASSOC);
+  } else {
+    $results = sql("SELECT * FROM fagi WHERE displayname='$displayname'","getAll",DB_FETCHMODE_ASSOC);
+  }
+  if(is_array($results)){
+    return $results;
+  } else { 
+    return null;
+  }
+}
+
+function modfagi_get($id){
+  //get all the variables for the timecondition
+  $results = sql("SELECT * FROM fagi WHERE id = '$id'","getRow",DB_FETCHMODE_ASSOC);
+  return $results;
+}
+
+function modfagi_del($id){
+  $results = sql("DELETE FROM fagi WHERE id = \"$id\"","query");
+}
+
+
+function modfagi_add($post){
+  global $amp_conf;  
+  extract($post);
+
+  if ($agiport =='') {
+    $agiport=(isset($amp_conf['FAGIPORT']))?$amp_conf['FAGIPORT']:'1985';
+  }
+  $results = sql("INSERT INTO fagi (displayname,description,host,port,path,query,truegoto,falsegoto) values 
+   ('$displayname','$agidesc','$agihost','$agiport','$agipath','$agiquery','${$goto0.'0'}','${$goto1.'1'}')");
+
+}
+
+function modfagi_edit($id,$post){
+  extract($post);
+
+  if ($agiport =='') {
+    $agiport=(isset($amp_conf['FAGIPORT']))?$amp_conf['FAGIPORT']:'1985';
+  }
+  $results = sql("UPDATE fagi set displayname='$displayname',description='$agidesc',host='$agihost',port='$agiport',path='$agipath',
+query='$agiquery',truegoto='${$goto0.'0'}',falsegoto='${$goto1.'1'}' where id='$id'"); 
+}
+
+?>
---
+++ page.fagi.php
@@ -0,0 +1,157 @@
+<?php /* $Id */
+//Copyright (C) 2004 Coalescent Systems Inc. (info@coalescentsystems.ca)
+//
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either version 2
+//of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+
+isset($_REQUEST['action'])?$action = $_REQUEST['action']:$action='';
+//the item we are currently displaying
+isset($_REQUEST['itemid'])?$itemid=mysql_real_escape_string($_REQUEST['itemid']):$itemid='';
+
+$dispnum = "fagi"; //used for switch on config.php
+$tabindex = 0;
+
+//if submitting form, update database
+switch ($action) {
+	case "add":
+		modfagi_add($_POST);
+		needreload();
+		redirect_standard();
+	break;
+	case "delete":
+		modfagi_del($itemid);
+		needreload();
+		redirect_standard();
+	break;
+	case "edit":  //just delete and re-add
+		modfagi_edit($itemid,$_POST);
+		needreload();
+		redirect_standard('itemid');
+	break;
+}
+
+
+//get list of time conditions
+$fagis = modfagi_list();
+?>
+
+</div> <!-- end content div so we can display rnav properly-->
+
+<!-- right side menu -->
+<div class="rnav"><ul>
+    <li><a id="<?php echo ($itemid=='' ? 'current':'') ?>" href="config.php?display=<?php echo urlencode($dispnum)?>"><?php echo _("Add Fast AGI")?></a></li>
+<?php
+if (isset($fagis)) {
+	foreach ($fagis as $fagi) {
+		echo "<li><a id=\"".($itemid==$fagi['id'] ? 'current':'')."\" href=\"config.php?display=".urlencode($dispnum)."&itemid=".urlencode($fagi['id'])."\">{$fagi['displayname']}</a></li>";
+	}
+}
+?>
+</ul></div>
+
+<div class="content">
+<?php
+if ($itemid){ 
+  $thisItem = modfagi_get($itemid);
+}
+?>
+  <h2><?php echo ($itemid ? _("Fast Agi:")." ". $thisItem['displayname'] : _("Fast Agi")); ?></h2>
+<?php		
+if ($itemid){ 
+  $delURL = $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'].'&action=delete';
+  $tlabel = sprintf(_("Delete Fast Agi: %s"),trim($thisItem['displayname']) == '' ? $itemid : $thisItem['displayname']." ($itemid) ");
+  $label = '<span><img width="16" height="16" border="0" title="'.$tlabel.'" alt="" src="images/core_delete.png"/>&nbsp;'.$tlabel.'</span>';
+?>
+  <a href="<?php echo $delURL ?>"><?php echo $label; ?></a><br />
+<?php
+  $usage_list = framework_display_destination_usage(modfagi_getdest($itemid));
+  if (!empty($usage_list)) {
+?>
+  <a href="#" class="info"><?php echo $usage_list['text']?>:<span><?php echo $usage_list['tooltip']?></span></a>
+<?php
+  }
+} 
+?>
+<form autocomplete="off" name="fagi" action="<?php $_SERVER['PHP_SELF'] ?>" method="post" onsubmit="return checkName(fagi);">
+<input type="hidden" name="display" value="<?php echo $dispnum?>">
+<input type="hidden" name="action" value="<?php echo ($itemid ? 'edit' : 'add') ?>">
+<?php		if ($itemid){ ?>
+  <input type="hidden" name="account" value="<?php echo $itemid; ?>">
+<?php		}?>
+<table>
+<tr><td colspan="2"><h5><?php echo ($itemid ? _("Edit Fast AGI") : _("Add Fast AGI")) ?><hr></h5></td></tr>
+<tr>
+ <td><a href="#" class="info"><?php echo _("Name:")?><span><?php echo _("Give this Fast AGI a brief name to help you identify it.")?></span></a></td>
+ <td><input type="text" name="displayname" value="<?php echo (isset($thisItem['displayname']) ? $thisItem['displayname'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+</tr>
+<tr>
+ <td><a href="#" class="info"><?php echo _("Description:")?><span><?php echo _("Give the description to help you identify it.")?></span></a></td>
+ <td><input type="text" size="40" name="agidesc" value="<?php echo (isset($thisItem['description']) ? $thisItem['description'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+</tr>
+<tr>
+ <td><a href="#" class="info"><?php echo _("Host:")?><span><?php echo _("Host name or IP address")?></span></a></td>
+ <td><input type="text" name="agihost" value="<?php echo (isset($thisItem['host']) ? $thisItem['host'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+</tr>
+<tr>
+ <td><a href="#" class="info"><?php echo _("Port:")?><span><?php echo _("Port FAGI server is listening at (default 1585, is possible to change default in amportal.conf with FAGIPORT)")?></span></a></td>
+ <td><input type="text" name="agiport" value="<?php echo (isset($thisItem['port']) ? $thisItem['port'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+</tr>
+								
+<tr>
+ <td><a href="#" class="info"><?php echo _("Path:")?><span><?php echo _("Path for FAGI<br/>e.g.: /cidlookup")?></span></a></td>
+ <td><input type="text" name="agipath" value="<?php echo (isset($thisItem['path']) ? $thisItem['path'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+</tr>
+<tr>
+ <td><a href="#" class="info"><?php echo _("Query:")?><span><?php echo _("Query string")?></span></a></td>
+ <td><input type="text" name="agiquery" value="<?php echo (isset($thisItem['query']) ? $thisItem['query'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+</tr>
+
+<tr><td colspan="2"><br><h5><?php echo _("Destination if Fast AGI return")?><hr></h5></td></tr>
+<?php 
+//draw goto selects
+if (isset($thisItem)) {
+  echo drawselects($thisItem['truegoto'],0);
+} else { 
+  echo drawselects(null, 0);
+}
+?>
+
+<tr><td colspan="2"><br><h5><?php echo _("Destination if Fast AGI Error")?><hr></h5></td></tr>
+
+<?php 
+//draw goto selects
+if (isset($thisItem)) {
+  echo drawselects($thisItem['falsegoto'],1);
+} else { 
+  echo drawselects(null, 1);
+}
+?>
+
+<tr>
+ <td colspan="2"><br><h6><input name="Submit" type="submit" value="<?php echo _("Submit")?>" tabindex="<?php echo ++$tabindex;?>"></h6></td>		
+</tr>
+</table>
+</form>
+  <script language="javascript">
+  <!--  
+function checkName(theForm) {
+  var RegExPattern = /^[a-zA-Z][a-zA-z0-9]*$/;
+  var msgInvalidName = "<?php echo _("Please enter a valid name (just asci, with out &_' ..) "); ?>";
+  var name=theForm.displayname.value;
+
+  if (name.match(RegExPattern))
+   { 
+    return true;
+   }
+return warnInvalid(theForm.displayname, msgInvalidName);
+  }
+//-->
+</script>
