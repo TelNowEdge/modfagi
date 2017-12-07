fea7cca09279bb57b79cafcefc6129206df6db25
Boris Morel <boris.morel@telnowedge.com>
2017-12-07 15:34:27 +0100

    [FIX] Sql syntaxe

--- Resources/Migrations/FagiMigration.php
+++ Resources/Migrations/FagiMigration.php
@@ -6,12 +6,31 @@ use TelNowEdge\FreePBX\Base\Resources\Migrations\AbstractMigration;
 
 class FagiMigration extends AbstractMigration
 {
-    public function migration2017120701()
+    public function migration2017120702()
     {
         return '
 ALTER TABLE
-    `fagi` CHANGE COLUMN `truegoto` `truegoto` VARCHAR (255) DEFAULT NOT NULL
-    ,CHANGE COLUMN `falsegoto` `falsegoto` VARCHAR (255) DEFAULT NOT NULL
+    `fagi` CHANGE COLUMN `truegoto` `truegoto` VARCHAR (255) NOT NULL
+    ,CHANGE COLUMN `falsegoto` `falsegoto` VARCHAR (255) NOT NULL
+';
+    }
+
+    public function migration2017120701()
+    {
+        return '
+CREATE
+    TABLE
+        IF NOT EXISTS fagi (
+            id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT
+            ,displayname VARCHAR (32) NOT NULL
+            ,description VARCHAR (50) NOT NULL
+            ,HOST VARCHAR (30) DEFAULT NULL
+            ,port VARCHAR (30) DEFAULT NULL
+            ,PATH VARCHAR (100) DEFAULT NULL
+            ,query VARCHAR (100) DEFAULT NULL
+            ,truegoto VARCHAR (50) DEFAULT NULL
+            ,falsegoto VARCHAR (50) DEFAULT NULL
+        )
 ';
     }
 }
